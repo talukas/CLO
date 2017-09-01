559093fb108ca44dc181dc95900e2882861884be
Tanya Lukashuk <tlukashuk@l-c02mq52yfd57.wsgc.com>
2017-08-31 17:32:42 -0700

    Allmost done

--- Parse/src/test/java/XMLParser/Parse/XMLParserTest.java
+++ Parse/src/test/java/XMLParser/Parse/XMLParserTest.java
@@ -2,61 +2,33 @@ package XMLParser.Parse;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
-
-import static org.testng.Assert.assertEquals;
-import static org.testng.Assert.assertTrue;
-import static org.hamcrest.Matchers.containsInAnyOrder;
-import static org.hamcrest.Matchers.contains;
-import static org.hamcrest.CoreMatchers.allOf;
-import static org.hamcrest.CoreMatchers.instanceOf;
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.CoreMatchers.notNullValue;
-import static org.hamcrest.Matchers.hasEntry;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.*;
-import static org.testng.Assert.assertNotNull;
-import static org.testng.Assert.assertNull;
-
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.lang.reflect.Method;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Iterator;
-import java.util.LinkedHashMap;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.Map;
-import java.util.TreeMap;
-
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
-
-import org.hamcrest.Matcher;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
-import org.testng.collections.Maps;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
-public class XMLParserTest extends ParseXML {
+public class XMLParserTest {
 
 	NodeList nList;
 	Node nNode;
 	Element eElement;
 	String csvFile = "./resources/ServersList.csv";
-	Map<String, String> hmap;
-	Map<String, String> map;
+	Map<String, String> hmap = new HashMap<>();
 	String attribute_02 = "Name";
 	String attribute_04 = "Status";
 	String actualName;
@@ -66,96 +38,50 @@ public class XMLParserTest extends ParseXML {
 	String expectedStatus;
 	String Name;
 	String Status;
+
 	@BeforeTest
-	public void beforeMethod() throws ParserConfigurationException, SAXException, IOException { //THIS SHOULD ALLWAYS BE "beforeMethod" NOT "beforeClass" !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-																								//"beforeClass" ONLY UPLOADS DATA ONCE AND USES IT FOR ALL OF THE TEST
-																								// "beforeMethod" UPLOADS DATA for EVERY TEST STARTS SAPARETLY
+	public void beforeMethod() throws ParserConfigurationException, SAXException, IOException {
+
 		DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
 		DocumentBuilder dbBuilder = dbFactory.newDocumentBuilder();
 		File fXmlFile = new File("./resources/SterlingServices.xml");
 		if (fXmlFile.exists()) {
 			Document doc = dbBuilder.parse(fXmlFile);
-			Element docEle = doc.getDocumentElement();
+			// Element docEle = doc.getDocumentElement();
 
 			// Print root element of the document
-			System.out.println("Root element of the document: " + docEle.getNodeName());
+			// System.out.println("Root element of the document: " + docEle.getNodeName());
 
 			doc.getDocumentElement().normalize();
 
 			nList = doc.getElementsByTagName("Server");
 
-			// Print total elements in document
-			System.out.println("Total Servers: " + nList.getLength());
-
-			System.out.println("=====================");
-
-			// String attribute_01 = "Id";
 			String attribute_02 = "Name";
-			// String attribute_03 = "Type";
 			String attribute_04 = "Status";
 
 			if (nList != null && nList.getLength() > 0)
 				for (int i = 0; i < nList.getLength(); i++) {
 					nNode = nList.item(i);
 					eElement = (Element) nNode;
-					
-					
-					hmap = new HashMap<String, String>();
 
 					Name = eElement.getAttribute(attribute_02);
 					Status = eElement.getAttribute(attribute_04);
 					hmap.put(Name, Status);
-					
 
-					
-					for (String Key : hmap.keySet()) {
-						 actual = hmap.get(Key);
-						// System.out.println("Key actual :" + Key);
-						// System.out.println("Value actual : " + actual);
-						// System.out.println("******");
-					}
-					
+					hmap.get(Name);
+
 				}
-				}
-		
-		// List<String> value = new ArrayList<String>();
-
-		// value.add(eElement.getAttribute(attribute_01));
-		// value.add(eElement.getAttribute(attribute_03));
-		// value.add(eElement.getAttribute(attribute_04));
-		
-		
-		//System.out.println("Actual:" + " " + key + " " + value);
-
-     		
-		
 		}
-	
+	}
 
 	@Test(dataProvider = "dp")
-	public void convert(String Name, String Status) {
-		
-		map = new HashMap<String, String>();
-		map.put(Name, Status);
+	public void convert(String ServiceName, String ServiceStatusExpect) {
 
-		for (String Key : map.keySet()) {
-		 expected = map.get(Key);
-		// System.out.println("Key expected:" + Key);
-		// System.out.println("Value expected : " + expected);
-		}
-		
-		
+		String ServiseStatusActual = hmap.get(ServiceName);
 
-		assertThat(expected, is(actual));
-		
-		
+		assertThat(ServiseStatusActual, is("Active"));
 
-	
-		
-				}
-	
-	
-
+	}
 
 	@DataProvider(name = "dp")
 
@@ -178,7 +104,6 @@ public class XMLParserTest extends ParseXML {
 		}
 
 		br.close();
-
 		return al.iterator();
 	}
 
